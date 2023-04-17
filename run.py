7e3a242630bd70cb00ed9e4a82975f92813ad5fe
Grant Fitez <grantfitez@gmail.com>
2023-04-17 08:41:14 -0400

    new b data

--- run.py
+++ run.py
@@ -22,6 +22,6 @@ def run(params = {}):
 Tc=2.269
 
 
-for i in range(10):
+for i in range(100):
     print(i)
-    run({"t_step":0.01/3, "B":0.1, "flip_perc":0.1, "N":100, "t_min":1.2, "t_max":3.2, "n_burnin":60000, "n_steps":100000, "t_top":4, "B_top":1, "use_gaussian":False})
+    run({"t_step":0.01/3, "flip_perc":0.1, "N":100, "t_min":1.2, "t_max":3.2, "n_burnin":60000, "n_steps":100000, "t_top":4, "B_top":1, "use_gaussian":False})
